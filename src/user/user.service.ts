1df2cf67085a9f4a6c701c01c1fe1ee57fcf9a15
jihyeon.lim <sen03620@naver.com>
2024-01-01 18:37:27 +0900

    네이버 카카오 관련 함수 작성

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -54,6 +54,25 @@ export class UserService {
     return await this.userRepository.save(user);
   }
 
+  async naverSignUp(
+    naverId: string,
+    name: string,
+    email: string,
+    profile: string,
+    phone: string = '',
+  ) {
+    const id = uuid();
+    const user = await this.userRepository.create({
+      id,
+      naverId,
+      name,
+      phone,
+      email,
+      profile,
+    });
+    return await this.userRepository.save(user);
+  }
+
   async updateUser(id: string, data: UpdateUserDto) {
     const user = await this.getUserById(id);
 
@@ -96,8 +115,25 @@ export class UserService {
       deletedAt: IsNull(),
     });
   }
+  async getUserByNaverId(id: string) {
+    return await this.userRepository.findOneBy({
+      naverId: id,
+      deletedAt: IsNull(),
+    });
+  }
 
   async setRefreshToken(id: string, token: string) {
     this.userRepository.update(id, { refreshToken: token });
+  }
+
+  async kakaoNextSignUp(id: string, phone: string, name: string) {
+    const user = await this.userRepository.findOneBy({
+      id,
+      deletedAt: IsNull(),
+    });
+
+    if (!user) throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
+
+    return await this.userRepository.update(id, { phone, name });
   }
 }
