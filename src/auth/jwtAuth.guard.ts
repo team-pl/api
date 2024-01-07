77a50b0e70ef86c0a2fed5874cea1445b5a04aeb
jihyeon.lim <sen03620@naver.com>
2024-01-07 17:21:48 +0900

    JWT 방식 변경

--- src/auth/jwtAuth.guard.ts
+++ src/auth/jwtAuth.guard.ts
@@ -1,5 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { config } from 'dotenv';
+
+config();
 
 Injectable();
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private jwtService: JwtService) {
+    super();
+  }
+
+  validateToken(token: string) {
+    const secretKey = process.env.SECRET_KEY;
+
+    try {
+      const verify = this.jwtService.verify(token, { secret: secretKey });
+      console.log('verify : ', verify);
+      return verify;
+    } catch (e) {
+      switch (e.message) {
+        // 토큰에 대한 오류를 판단합니다.
+        case 'INVALID_TOKEN':
+        case 'TOKEN_IS_ARRAY':
+        case 'NO_USER':
+          throw new HttpException('유효하지 않은 토큰입니다.', 401);
+
+        case 'EXPIRED_TOKEN':
+          throw new HttpException('토큰이 만료되었습니다.', 410);
+
+        default:
+          throw new HttpException('서버 오류입니다.', 500);
+      }
+    }
+  }
+}
