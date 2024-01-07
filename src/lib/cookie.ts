a4a145c0217876a9760a31f1adf9a5ef9445b451
jihyeon.lim <sen03620@naver.com>
2024-01-07 17:22:09 +0900

    쿠키 옵션 추가

---
+++ src/lib/cookie.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+
+config();
+
+export const cookieOption = {
+  secure: process.env.NODE_ENV === 'development' ? false : true,
+  domain:
+    process.env.NODE_ENV === 'development'
+      ? 'localhost'
+      : 'www.api.teampl-plus.com',
+  maxAge: 7 * 24 * 60 * 60 * 1000, //7d,
+  path: '/',
+
+  // httpOnly: false,
+  // signed: true,
+};
