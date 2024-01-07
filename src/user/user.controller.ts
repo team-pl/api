9677e36a2ac08dd2fd0524c4b789d5e6824f7239
jihyeon.lim <sen03620@naver.com>
2024-01-07 17:22:35 +0900

    signup API 수정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,9 +3,13 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Post,
   Put,
+  Request,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
@@ -19,6 +23,7 @@ import {
   SwaggerPostResponse,
 } from 'src/decorator/swagger.decorator';
 import { SignupKakaoUserDto } from './dto/signup-user.dto';
+import { JwtAuthGuard } from 'src/auth/jwtAuth.guard';
 
 @Controller('user')
 @ApiExtraModels(GetUserResDto, PostUserResDto)
@@ -45,21 +50,26 @@ export class UserController {
     return this.service.signUp(data);
   }
 
+  @Put('/signup')
+  @UseGuards(JwtAuthGuard)
+  signUpKakaoUser(
+    @Request() req,
+    @Body(new ValidationPipe()) data: SignupKakaoUserDto,
+  ) {
+    const { id } = req.user.name;
+    const { phone, name } = data;
+    if (!id) {
+      throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
+    }
+    return this.service.kakaoNextSignUp(id, phone, name);
+  }
+
   @Put(':id')
   updateUser(
     @Param('id') id: string,
     @Body(new ValidationPipe()) data: UpdateUserDto,
   ) {
     return this.service.updateUser(id, data);
-  }
-
-  @Put('signup/:id')
-  signUpKakaoUser(
-    @Param('id') id: string,
-    @Body(new ValidationPipe()) data: SignupKakaoUserDto,
-  ) {
-    const { phone, name } = data;
-    return this.service.kakaoNextSignUp(id, phone, name);
   }
 
   @Delete(':id')
