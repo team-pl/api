566e990aa5aec44b178fac47032929acbe67d2be
jihyeon.lim <sen03620@naver.com>
2024-01-20 18:11:14 +0900

    project 모듈에 file 모듈 추가

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -3,9 +3,10 @@ import { ProjectController } from './project.controller';
 import { ProjectService } from './project.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from 'src/entity/project.entity';
+import { FileModule } from 'src/file/file.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project])],
+  imports: [TypeOrmModule.forFeature([Project]), FileModule],
   controllers: [ProjectController],
   providers: [ProjectService],
   exports: [ProjectService],
