7f39517ccd040a1278270fe32790e60cecb93971
jihyeon.lim <sen03620@naver.com>
2024-01-30 21:49:22 +0900

    프로젝트 등록 API 수정

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -4,24 +4,28 @@ import { Project } from 'src/entity/project.entity';
 import { Repository } from 'typeorm';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { v4 as uuid } from 'uuid';
+import { ProfileService } from 'src/profile/profile.service';
 
 @Injectable()
 export class ProjectService {
   constructor(
     @InjectRepository(Project)
     private projectRepository: Repository<Project>,
+    private readonly profileService: ProfileService,
   ) {}
 
   async create(data: CreateProjectDto, userId: string, fileUrl: string | null) {
     const id = uuid();
 
-    const { recruitExpiredAt, content, file, ...rest } = data;
+    const { recruitExpiredAt, content, file, profileId, ...rest } = data;
 
-    const [year, month, date] = recruitExpiredAt.split('-').map(Number);
+    const [year, month, date] = recruitExpiredAt.split('/').map(Number);
 
     const expiredAt = new Date(year, month - 1, date, 23, 59, 59);
 
     const bufferContent = Buffer.from(content, 'utf-8');
+
+    const profileData = await this.profileService.getProfileById(profileId);
 
     const project = await this.projectRepository.create({
       id,
@@ -32,6 +36,12 @@ export class ProjectService {
       ...rest,
     });
 
-    return await this.projectRepository.save(project);
+    const result = await this.projectRepository.save(project);
+
+    return {
+      ...result,
+      content: result.content.toString(),
+      profile: profileData,
+    };
   }
 }
