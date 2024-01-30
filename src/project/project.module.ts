e8680044c4bdbad3c9024c70aa0882c41b71d840
jihyeon.lim <sen03620@naver.com>
2024-01-30 21:48:57 +0900

    프로젝트 모듈에 프로필 모듈 import 추가

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -4,9 +4,10 @@ import { ProjectService } from './project.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from 'src/entity/project.entity';
 import { FileModule } from 'src/file/file.module';
+import { ProfileModule } from 'src/profile/profile.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project]), FileModule],
+  imports: [TypeOrmModule.forFeature([Project]), FileModule, ProfileModule],
   controllers: [ProjectController],
   providers: [ProjectService],
   exports: [ProjectService],
