74ac557d83e428303edbb9cfea1a709d179d1198
jihyeon.lim <sen03620@naver.com>
2024-02-04 15:38:30 +0900

    profile 모듈에 user 모듈 추가

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -4,9 +4,10 @@ import { ProfileController } from './profile.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Profile } from 'src/entity/profile.entity';
 import { FileModule } from 'src/file/file.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Profile]), FileModule],
+  imports: [TypeOrmModule.forFeature([Profile]), FileModule, UserModule],
   providers: [ProfileService],
   controllers: [ProfileController],
   exports: [ProfileService],
