5833a516099d8a9b82bb2038f7eb5026f75de7ed
jihyeon.lim <sen03620@naver.com>
2024-02-04 15:39:04 +0900

    profile POST API에 로직 추가

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,12 +4,14 @@ import { Profile } from 'src/entity/profile.entity';
 import { IsNull, Repository } from 'typeorm';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { v4 as uuid } from 'uuid';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(Profile)
     private profileRepository: Repository<Profile>,
+    private readonly userService: UserService,
   ) {}
 
   async create(data: CreateProfileDto, userId: string, fileUrl: string | null) {
@@ -17,14 +19,48 @@ export class ProfileService {
 
     const { portfolioFile, ...rest } = data;
 
-    const project = await this.profileRepository.create({
+    // NOTE: 아존애 둥록한 프로필 SELECT
+    const prevProfiles = await this.profileRepository.find({
+      where: { deletedAt: IsNull(), userId },
+    });
+
+    // NOTE: 이전에 등록한 프로필 개수가 3개 이성아면 등록을 못하도록 함
+    if (prevProfiles.length >= 3) {
+      throw new HttpException(
+        'Number of profile registrations exceeded',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    // NOTE: 현재 등록한 프로필을 대표 프로필로 지정하려고 할때
+    if (rest.isRepresentative) {
+      // NOTE: 이전에 등록한 프로필 중에서 대표 프로필이 있는지 확인
+      const prevRepresentative = prevProfiles.filter(
+        (data) => data.isRepresentative,
+      );
+
+      if (prevRepresentative.length > 0) {
+        // NOTE: 기존 대표 프로필을 기본 프로필로 변경
+        await this.profileRepository.update(prevRepresentative[0].id, {
+          isRepresentative: false,
+        });
+      }
+
+      // NOTE: user 테이블에서 대표 프로필 ID 수정
+      await this.userService.updateUser(userId, {
+        representativeProfileId: id,
+      });
+    }
+
+    // NOTE: 프로필 생성
+    const profile = await this.profileRepository.create({
       id,
       userId,
       portfolioFile: fileUrl,
       ...rest,
     });
 
-    return await this.profileRepository.save(project);
+    return await this.profileRepository.save(profile);
   }
 
   async getProfileById(id: string) {
@@ -33,7 +69,8 @@ export class ProfileService {
       deletedAt: IsNull(),
     });
 
-    if (!profileData) throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
+    if (!profileData)
+      throw new HttpException('Profile NotFound', HttpStatus.NOT_FOUND);
 
     return profileData;
   }
