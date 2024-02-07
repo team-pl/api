d9b3bc7be37f2a84a92f394c0d6e84b5f2d63eb4
jihyeon.lim <sen03620@naver.com>
2024-02-07 22:48:45 +0900

    프로젝트 module 추가

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -5,9 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from 'src/entity/project.entity';
 import { FileModule } from 'src/file/file.module';
 import { ProfileModule } from 'src/profile/profile.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project]), FileModule, ProfileModule],
+  imports: [
+    TypeOrmModule.forFeature([Project]),
+    FileModule,
+    ProfileModule,
+    UserModule,
+  ],
   controllers: [ProjectController],
   providers: [ProjectService],
   exports: [ProjectService],
