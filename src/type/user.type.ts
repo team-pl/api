b15b4ae4f2340c18f4d900074430eb52b31cc7d5
jihyeon.lim <sen03620@naver.com>
2024-02-07 22:50:53 +0900

    사용자 enum 수정

--- src/type/user.type.ts
+++ src/type/user.type.ts
@@ -14,9 +14,4 @@ enum ESignUp {
   KAKAO = 'KAKAO', // 카카오
 }
 
-enum EJob {
-  DESIGN = 'DESIGNER', // 디자이너
-  ENGINEER = 'ENGINEER', // 엔지니어
-}
-
-export { TAuthUser, TLoginUser, ESignUp, EJob };
+export { TAuthUser, TLoginUser, ESignUp };
