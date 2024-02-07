681243ef0530e43b3b778b19004d2c6e694baafd
jihyeon.lim <sen03620@naver.com>
2024-02-07 22:49:28 +0900

    프로젝트 등록 로직 수정 및 홈 GET API 로직 추가

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Project } from 'src/entity/project.entity';
-import { Repository } from 'typeorm';
+import { IsNull, Repository } from 'typeorm';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { v4 as uuid } from 'uuid';
 import { ProfileService } from 'src/profile/profile.service';
+import { ECategory1 } from 'src/type/project.type';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class ProjectService {
@@ -12,9 +14,15 @@ export class ProjectService {
     @InjectRepository(Project)
     private projectRepository: Repository<Project>,
     private readonly profileService: ProfileService,
+    private readonly userService: UserService,
   ) {}
 
   async create(data: CreateProjectDto, userId: string, fileUrl: string | null) {
+    let devCount = 0;
+    let designCount = 0;
+    let etcCount = 0;
+    const category: ECategory1[] = [];
+
     const id = uuid();
 
     const { recruitExpiredAt, content, file, profileId, ...rest } = data;
@@ -29,12 +37,42 @@ export class ProjectService {
 
     const profileData = await this.profileService.getProfileById(profileId);
 
+    const userNickname = await this.userService.getUserNicknameById(userId);
+
+    for (let i = 1; i <= 10; i++) {
+      if (!rest[`category${i}_1`]) break;
+
+      if (rest[`category${i}_1`] === ECategory1.DEVELOPER)
+        devCount += rest[`category${i}Number`];
+      else if (rest[`category${i}_1`] === ECategory1.DESIGN)
+        designCount += rest[`category${i}Number`];
+      else etcCount += rest[`category${i}Number`];
+    }
+
+    if (devCount > 0) {
+      category.push(ECategory1.DEVELOPER);
+    }
+
+    if (designCount > 0) {
+      category.push(ECategory1.DESIGN);
+    }
+
+    if (etcCount > 0) {
+      category.push(ECategory1.ETC);
+    }
+
     const project = await this.projectRepository.create({
       id,
       recruitExpiredAt: expiredAt,
       content: bufferContent,
       userId,
       file: fileUrl,
+      profileId,
+      recruitDevTotalNumber: devCount,
+      recruitDesignTotalNumber: designCount,
+      recruitEtcTotalNumber: etcCount,
+      recruitCategory: category.join('/'),
+      userName: userNickname,
       ...rest,
     });
 
@@ -44,6 +82,47 @@ export class ProjectService {
       ...result,
       content: result.content.toString(),
       profile: profileData,
+    };
+  }
+
+  async getHomeProject() {
+    // NOTE: 인기 점수 내림차순으로 프로젝트 8개를 가져옴
+    const popularList = await this.projectRepository.findAndCount({
+      where: { deletedAt: IsNull() },
+      take: 8,
+      order: { numberOfScore: 'DESC' },
+      select: [
+        'name',
+        'content',
+        'state',
+        'recruitCategory',
+        'recruitTotalNumber',
+        'confirmedNumber',
+        'userName',
+        'recruitExpiredAt',
+      ],
+    });
+
+    // NOTE: 프로젝트 생성일 내림차순으로 프로젝트 8개를 가져옴
+    const newList = await this.projectRepository.findAndCount({
+      where: { deletedAt: IsNull() },
+      take: 8,
+      select: [
+        'name',
+        'content',
+        'state',
+        'recruitCategory',
+        'recruitTotalNumber',
+        'confirmedNumber',
+        'userName',
+        'recruitExpiredAt',
+      ],
+      order: { createdAt: 'DESC' },
+    });
+
+    return {
+      popular: popularList,
+      new: newList,
     };
   }
 }
