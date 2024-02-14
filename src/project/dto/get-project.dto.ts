c4f1fdd6bd43cb903c2406f2770e066020488bcc
jihyeon.lim <sen03620@naver.com>
2024-02-14 22:43:17 +0900

    GET /project DTO 작성

---
+++ src/project/dto/get-project.dto.ts
@@ -0,0 +1,34 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ECategorySelect } from 'src/type/project.type';
+
+export class GetProjectQueryDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: '현재까지 조회한 프로젝트 개수',
+  })
+  skip: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({
+    description: '조회하려는 프로젝트 개수',
+  })
+  take?: string;
+
+  @IsOptional()
+  @IsEnum(ECategorySelect)
+  @ApiPropertyOptional({
+    description: '조회 카테고리',
+    enum: ECategorySelect,
+  })
+  category?: ECategorySelect;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({
+    description: '검색어',
+  })
+  searchWord?: string;
+}
