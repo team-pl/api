bc081e4c29a916e0b5822d4962761b2865858f74
jihyeon.lim <sen03620@naver.com>
2024-02-20 21:46:00 +0900

    안 사용하는 ApiProperty 제거

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
 
 export class UpdateUserDto {
