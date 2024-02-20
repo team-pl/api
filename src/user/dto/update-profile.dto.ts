7c41d5186abc55c28e65684aef825d66dd62e1e2
jihyeon.lim <sen03620@naver.com>
2024-02-20 21:45:20 +0900

    프로필 수정 dto 추가

---
+++ src/user/dto/update-profile.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class UpdateProfileDto {
+  @IsOptional()
+  @ApiPropertyOptional({
+    type: 'string',
+    format: 'binary',
+    description: '프로필 이미지',
+  })
+  file: Express.Multer.File;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: '닉네임',
+  })
+  nickname: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: '직책 혹은 전공',
+    example: '엔지니어',
+  })
+  jobType: string;
+}
