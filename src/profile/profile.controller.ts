90273e45197d39e92a472ac686f9fec6af7a48d5
jihyeon.lim <sen03620@naver.com>
2024-02-20 21:33:52 +0900

    프로필 수정 및 삭제 API 추가

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,8 +1,11 @@
 import {
   Body,
   Controller,
+  Delete,
   HttpException,
   HttpStatus,
+  Param,
+  Patch,
   Post,
   Request,
   UseGuards,
@@ -12,6 +15,8 @@ import {
 import {
   ApiConsumes,
   ApiExtraModels,
+  ApiOperation,
+  ApiParam,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
@@ -21,10 +26,21 @@ import { JwtAuthGuard } from 'src/auth/jwtAuth.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { EFileUsage } from 'src/type/file.type';
-import { PostProfileResDto } from './dto/response.dto';
+import {
+  DeleteProfileResDto,
+  PostProfileResDto,
+  UpdateProfileResDto,
+} from './dto/response.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profile')
-@ApiExtraModels(CreateProfileDto, PostProfileResDto)
+@ApiExtraModels(
+  CreateProfileDto,
+  PostProfileResDto,
+  DeleteProfileResDto,
+  UpdateProfileDto,
+  UpdateProfileResDto,
+)
 @ApiTags('Profile')
 export class ProfileController {
   constructor(
@@ -34,6 +50,7 @@ export class ProfileController {
 
   @Post()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: '프로필 등록 API' })
   @ApiConsumes('multipart/form-data')
   @UseInterceptors(FileInterceptor('file'))
   @ApiResponse({
@@ -72,5 +89,65 @@ export class ProfileController {
     }
 
     return this.service.create(data, id, fileUrl);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: '프로필 수정 API' })
+  @UseGuards(JwtAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '수정할 프로필 ID',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'user ID NotFound',
+  })
+  @ApiResponse({
+    status: 200,
+    type: UpdateProfileResDto,
+  })
+  async update(
+    @Request() req,
+    @Body(new ValidationPipe()) data: UpdateProfileDto,
+    @Param('id') profileId: string,
+  ) {
+    const { id } = req.user.name;
+
+    const { portfolioFile } = data;
+
+    if (!id) {
+      throw new HttpException('NotFound', HttpStatus.UNAUTHORIZED);
+    }
+
+    let fileUrl: string | null = null;
+
+    if (portfolioFile) {
+      const uploadedFile = await this.fileService.uploadFile(
+        portfolioFile,
+        id,
+        EFileUsage.PROFILE,
+      );
+      fileUrl = uploadedFile.url;
+    }
+
+    return this.service.updateProfile(profileId, data, id, fileUrl);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '프로필 삭제 API' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '삭제할 프로필 ID',
+  })
+  @ApiResponse({
+    status: 200,
+    type: DeleteProfileResDto,
+  })
+  deleteUser(@Param('id') id: string) {
+    return this.service.deleteProfile(id);
   }
 }
