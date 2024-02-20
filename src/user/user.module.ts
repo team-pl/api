efa891bd72fc2839a6d912669cc628d4daa61c37
jihyeon.lim <sen03620@naver.com>
2024-02-20 21:43:20 +0900

    파일 모듈 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { FileModule } from 'src/file/file.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), FileModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
