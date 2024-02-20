da04f5a739bc065164db64002300e52bb2b33896
jihyeon.lim <sen03620@naver.com>
2024-02-20 21:44:25 +0900

    프로필사진이 없을때 랜덤이미지 적용 및 프로필 조회 및 수정 로직 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,12 +12,17 @@ import { IsNull, Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import { ESignUp } from 'src/type/user.type';
 import { SignupUserDto } from './dto/signup-user.dto';
+import { randomProfile } from 'src/lib/random-profile';
+import { FileService } from 'src/file/file.service';
+import { EFileUsage } from 'src/type/file.type';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private readonly fileService: FileService,
   ) {}
 
   async getAllUser() {
@@ -44,13 +49,20 @@ export class UserService {
     profileImageUrl: string,
   ) {
     const id = uuid();
+
+    let url = profileImageUrl;
+
+    // NOTE: 프로필 사진이 없을떄 랜덤 이미지로 설정
+    if (!profileImageUrl) {
+      url = randomProfile();
+    }
     const user = await this.userRepository.create({
       id,
       kakaoId,
       name,
       phone: '',
       email,
-      profileImageUrl,
+      profileImageUrl: url,
       signUpType: ESignUp.KAKAO,
     });
     return await this.userRepository.save(user);
@@ -64,13 +76,21 @@ export class UserService {
     phone: string = '',
   ) {
     const id = uuid();
+
+    let url = profileImageUrl;
+
+    // NOTE: 프로필 사진이 없을떄 랜덤 이미지로 설정
+    if (!profileImageUrl) {
+      url = randomProfile();
+    }
+
     const user = await this.userRepository.create({
       id,
       naverId,
       name,
       phone,
       email,
-      profileImageUrl,
+      profileImageUrl: url,
       signUpType: ESignUp.NAVER,
     });
     return await this.userRepository.save(user);
@@ -93,7 +113,7 @@ export class UserService {
     return await this.userRepository.update(id, { deletedAt: now });
   }
 
-  async getUserById(id: string) {
+  async getUserById(id: string): Promise<User> {
     return await this.userRepository.findOneBy({
       id,
       deletedAt: IsNull(),
@@ -167,5 +187,60 @@ export class UserService {
     });
 
     return user.nickname;
+  }
+
+  async getMyInfo(id: string) {
+    const user = await this.userRepository.findOne({
+      where: { deletedAt: IsNull(), id },
+      select: [
+        'id',
+        'profileImageUrl',
+        'nickname',
+        'signUpType',
+        'jobType',
+        'representativeProfileId',
+      ],
+      relations: ['profile'],
+    });
+
+    return user;
+  }
+
+  async updateProfile(id: string, data: UpdateProfileDto) {
+    const { file, nickname, jobType } = data;
+
+    let fileUrl: string | null = null;
+
+    if (file) {
+      const uploadedFile = await this.fileService.uploadFile(
+        file,
+        id,
+        EFileUsage.DEFAULT,
+      );
+      fileUrl = uploadedFile.url;
+    }
+
+    if (fileUrl) {
+      await this.userRepository.update(id, {
+        profileImageUrl: fileUrl,
+        nickname,
+        jobType,
+      });
+    } else {
+      await this.userRepository.update(id, {
+        nickname,
+        jobType,
+      });
+    }
+
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+        deletedAt: IsNull(),
+      },
+      select: ['id', 'profileImageUrl', 'nickname', 'jobType'],
+    });
+
+    return user;
   }
 }
