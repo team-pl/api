c73c5629879b85087133f255f86afb0712028783
jihyeon.lim <sen03620@naver.com>
2024-02-24 16:14:51 +0900

    프로젝트 하위 카테고리 타입 추가

--- src/type/project.type.ts
+++ src/type/project.type.ts
@@ -41,4 +41,35 @@ enum ECategorySelect {
   ETC = 'ETC', // 기타
 }
 
-export { ECategory1, ECategory2, EProjectState, ECategorySelect };
+// NOTE: 프로젝트 찾기 페이지에서 하위 카테고리별로 검색할 때 사용
+enum ESubCategorySelect {
+  ALL = 'ALL', // 전체
+  DEV_BE = 'DEV_BE', // 백엔드
+  DEV_FE = 'DEV_FE', // 프론트엔드
+  DEV_FULL_STACK = 'DEV_FULL_STACK', // 웹 풀스택
+  DEV_ANDROID = 'DEV_ANDROID', // 안드로이드
+  DEV_IOS = 'DEV_IOS', // IOS
+  DEV_GAME_CLIENT = 'DEV_GAME_CLIENT', // 게임 클라이언트
+  DEV_GAME_SERVER = 'DEV_GAME_SERVER', // 게임 서버
+  DEV_DATA = 'DEV_DATA', // 데이터
+  DEV_AI = 'DEV_AI', // 인공지능/머신러닝
+  DEV_ETC = 'DEV_ETC', // 기타 개발분야
+  DESIGN_UX = 'DESIGN_UX', // UX디자이너
+  DESIGN_GUI = 'DESIGN_GUI', // GUI디자이너
+  DESIGN_GRAPHIC = 'DESIGN_GRAPHIC', // 그래픽 디자이너
+  DESIGN_BRAND = 'DESIGN_BRAND', // 브랜드 디자이너
+  DESIGN_VIDEO = 'DESIGN_VIDEO', // 영상 디자이너
+  DESIGN_3D = 'DESIGN_3D', // 3D 디자이너
+  DESIGN_BI = 'DESIGN_BI', // BI(Brand identity) 디자이너
+  DESIGN_BE = 'DESIGN_BE', // BE(Brand experience) 디자이너
+  DESIGN_ETC = 'DESIGN_ETC', // 기타 디자인분야
+  ETC = 'ETC', // 기타
+}
+
+export {
+  ECategory1,
+  ECategory2,
+  EProjectState,
+  ECategorySelect,
+  ESubCategorySelect,
+};
