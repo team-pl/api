8a91b0ebd36e9ed3491ac76468deacfdad692f57
jihyeon.lim <sen03620@naver.com>
2024-02-24 18:03:00 +0900

    프로젝트 수정 및 상세 조회API 추가

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,9 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpException,
   HttpStatus,
+  Param,
+  Patch,
   Post,
   Query,
   Request,
@@ -15,6 +18,7 @@ import {
   ApiConsumes,
   ApiExtraModels,
   ApiOperation,
+  ApiParam,
   ApiQuery,
   ApiResponse,
   ApiTags,
@@ -22,9 +26,12 @@ import {
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import {
+  DeleteProjectResDto,
+  GetOneProjectResDto,
   HomeProjectResDto,
   PostProjectResDto,
   ProjectDataResDto,
+  UpdateProjectResDto,
 } from './dto/response.dto';
 import { JwtAuthGuard } from 'src/auth/jwtAuth.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -32,6 +39,7 @@ import { FileService } from 'src/file/file.service';
 import { EFileUsage } from 'src/type/file.type';
 import { GetProjectQueryDto } from './dto/get-project.dto';
 import { ECategorySelect, ESubCategorySelect } from 'src/type/project.type';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Controller('project')
 @ApiExtraModels(
@@ -40,6 +48,9 @@ import { ECategorySelect, ESubCategorySelect } from 'src/type/project.type';
   HomeProjectResDto,
   GetProjectQueryDto,
   ProjectDataResDto,
+  DeleteProjectResDto,
+  UpdateProjectResDto,
+  GetOneProjectResDto,
 )
 @ApiTags('Project')
 export class ProjectController {
@@ -89,6 +100,55 @@ export class ProjectController {
     }
 
     return this.service.create(data, id, fileUrl);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: '프로젝트 수정 API' })
+  @UseGuards(JwtAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '수정할 프로젝트 ID',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'user ID NotFound',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Profile NotFound',
+  })
+  @ApiResponse({
+    status: 200,
+    type: UpdateProjectResDto,
+  })
+  async update(
+    @Request() req,
+    @Body(new ValidationPipe()) data: UpdateProjectDto,
+    @Param('id') projectId: string,
+  ) {
+    const { id } = req.user.name;
+
+    const { file } = data;
+
+    if (!id) {
+      throw new HttpException('NotFound', HttpStatus.UNAUTHORIZED);
+    }
+
+    let fileUrl: string | null = null;
+
+    if (file) {
+      const uploadedFile = await this.fileService.uploadFile(
+        file,
+        id,
+        EFileUsage.PROJECT,
+      );
+      fileUrl = uploadedFile.url;
+    }
+
+    return this.service.updateProject(projectId, data, fileUrl);
   }
 
   @Get()
@@ -151,5 +211,39 @@ export class ProjectController {
   })
   async getHomeProject() {
     return this.service.getHomeProject();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: '프로젝트 상세 조회 API' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '조회할 프로젝트 ID',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Project NotFound',
+  })
+  @ApiResponse({
+    status: 200,
+    type: GetOneProjectResDto,
+  })
+  getOneProject(@Param('id') id: string) {
+    return this.service.getOneProject(id);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '프로젝트 삭제 API' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '삭제할 프로젝트 ID',
+  })
+  @ApiResponse({
+    status: 200,
+    type: DeleteProjectResDto,
+  })
+  deleteUser(@Param('id') id: string) {
+    return this.service.deleteProfile(id);
   }
 }
