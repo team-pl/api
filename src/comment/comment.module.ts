53971d55347a738ff463a1de454bbc6243d6befe
jihyeon.lim <sen03620@naver.com>
2024-02-27 22:39:12 +0900

    comment module 추가

---
+++ src/comment/comment.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { CommentController } from './comment.controller';
+import { CommentService } from './comment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from 'src/entity/comment.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Comment])],
+  controllers: [CommentController],
+  providers: [CommentService],
+  exports: [CommentService],
+})
+export class CommentModule {}
