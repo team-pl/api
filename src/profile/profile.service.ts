7caa4f3d68c13fd218ec751e2603d14670eca017
jihyeon.lim <sen03620@naver.com>
2024-03-03 14:20:37 +0900

    프로필 임시저장 API 로직 추가 및 등록 로직 수정 및 임시저장 여부 값 return 추가

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -6,6 +6,7 @@ import { CreateProfileDto } from './dto/create-profile.dto';
 import { v4 as uuid } from 'uuid';
 import { UserService } from 'src/user/user.service';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { CreateProfileTempDto } from './dto/create-profile-temporary.dto';
 
 @Injectable()
 export class ProfileService {
@@ -18,15 +19,15 @@ export class ProfileService {
   async create(data: CreateProfileDto, userId: string, fileUrl: string | null) {
     const id = uuid();
 
-    const { portfolioFile, skill, edu, career, ...rest } = data;
+    const { portfolioFile, skill, edu, career, tempProfileId, ...rest } = data;
 
     // NOTE: 이전에 둥록한 프로필 SELECT
     const prevProfiles = await this.profileRepository.find({
       where: { deletedAt: IsNull(), userId },
     });
 
-    // NOTE: 이전에 등록한 프로필 개수가 3개 이성아면 등록을 못하도록 함
-    if (prevProfiles.length >= 3) {
+    // NOTE: 이전에 등록한 프로필 개수가 3개 이상 (+ 임시저장 글 등록X)일때 등록을 못하도록 함
+    if (prevProfiles.length >= 3 && !tempProfileId) {
       throw new HttpException(
         'Number of profile registrations exceeded',
         HttpStatus.FORBIDDEN,
@@ -41,8 +42,9 @@ export class ProfileService {
       );
 
       if (prevRepresentative.length > 0) {
+        const idList = prevRepresentative.map((data) => data.id);
         // NOTE: 기존 대표 프로필을 기본 프로필로 변경
-        await this.profileRepository.update(prevRepresentative[0].id, {
+        await this.profileRepository.update(idList, {
           isRepresentative: false,
         });
       }
@@ -53,10 +55,18 @@ export class ProfileService {
       });
     }
 
+    // NOTE: 현재 등록하려는 프로필이 임시저장된 프로필일때 기존 임시저장된 프로필 제거
+    if (tempProfileId) {
+      const now = new Date();
+      await this.profileRepository.update(tempProfileId, {
+        deletedAt: now,
+      });
+    }
+
     const eduData = {};
     const careerData = {};
 
-    edu.map((data, index) => {
+    edu?.map((data, index) => {
       eduData[`eduCategory${index + 1}`] = data.category;
       eduData[`eduSchoolName${index + 1}`] = data.schoolName;
       eduData[`eduMajor${index + 1}`] = data.major;
@@ -67,7 +77,7 @@ export class ProfileService {
         : true;
     });
 
-    career.map((data, index) => {
+    career?.map((data, index) => {
       careerData[`careerCompanyName${index + 1}`] = data.companyName;
       careerData[`careerRole${index + 1}`] = data.role;
       careerData[`careerJoinDate${index + 1}`] = data.joinDate;
@@ -94,12 +104,125 @@ export class ProfileService {
       userId: resultData.userId,
       name: resultData.name,
       isRepresentative: resultData.isRepresentative,
-      edu,
-      career,
-      skill,
+      edu: edu ? edu : [],
+      career: career ? career : [],
+      skill: skill ? skill : [],
       portfolioUrl: resultData.portfolioUrl,
       portfolioFile: fileUrl,
+      isTemporaryStorage: resultData.isTemporaryStorage,
     };
+  }
+
+  async temporaryStorage(
+    data: CreateProfileTempDto,
+    userId: string,
+    fileUrl: string | null,
+  ) {
+    const { portfolioFile, skill, edu, career, ...rest } = data;
+
+    // NOTE: 이전에 둥록한 프로필 SELECT
+    const prevProfiles = await this.profileRepository.find({
+      where: { deletedAt: IsNull(), userId },
+    });
+
+    // NOTE: 이전에 등록한 프로필 개수가 3개 이성아면 등록을 못하도록 함
+    if (prevProfiles.length >= 3) {
+      throw new HttpException(
+        'Number of profile registrations exceeded',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    const eduData = {};
+    const careerData = {};
+
+    edu?.map((data, index) => {
+      eduData[`eduCategory${index + 1}`] = data.category;
+      eduData[`eduSchoolName${index + 1}`] = data.schoolName;
+      eduData[`eduMajor${index + 1}`] = data.major;
+      eduData[`eduAdmissionDate${index + 1}`] = data.admissionDate;
+      eduData[`eduGraduationDate${index + 1}`] = data.graduationDate;
+      eduData[`eduIsAttending${index + 1}`] = data.graduationDate
+        ? false
+        : true;
+    });
+
+    career?.map((data, index) => {
+      careerData[`careerCompanyName${index + 1}`] = data.companyName;
+      careerData[`careerRole${index + 1}`] = data.role;
+      careerData[`careerJoinDate${index + 1}`] = data.joinDate;
+      careerData[`careerQuitDate${index + 1}`] = data.quitDate;
+      careerData[`careerIsWorking${index + 1}`] = data.quitDate ? false : true;
+    });
+
+    if (data.id) {
+      // NOTE: 프로필 임시저장 최초 생성 X
+      await this.profileRepository.update(data.id, {
+        portfolioFile: fileUrl,
+        skill: skill && skill.length ? skill.join('&&') : '',
+        ...eduData,
+        ...careerData,
+        ...rest,
+        isTemporaryStorage: true,
+      });
+
+      const resultData = await this.getProfileById(data.id);
+
+      return {
+        id: resultData.id,
+        createdAt: resultData.createdAt,
+        userId: resultData.userId,
+        name: resultData.name,
+        isRepresentative: resultData.isRepresentative,
+        edu: edu ? edu : [],
+        career: career ? career : [],
+        skill: skill ? skill : [],
+        portfolioUrl: resultData.portfolioUrl,
+        portfolioFile: fileUrl,
+        isTemporaryStorage: resultData.isTemporaryStorage,
+      };
+    } else {
+      // NOTE: 프로필 임시저장 최초 생성
+
+      const id = uuid();
+      let name = '';
+
+      if (data.name) {
+        // NOTE: 프로필 이름이 있을때
+        name = data.name;
+      } else {
+        // NOTE: 프로필 이름이 없을때
+        name = `임시저장 ${(await this.getTempLenthByUserId(userId)) + 1}`;
+      }
+
+      const profile = await this.profileRepository.create({
+        id,
+        userId,
+        portfolioFile: fileUrl,
+        skill: skill && skill.length ? skill.join('&&') : '',
+        ...eduData,
+        ...careerData,
+        ...rest,
+        name,
+        isTemporaryStorage: true,
+      });
+
+      const resultData = await this.profileRepository.save(profile);
+
+      return {
+        id: resultData.id,
+        createdAt: resultData.createdAt,
+        userId: resultData.userId,
+        name: resultData.name,
+        isRepresentative: resultData.isRepresentative,
+        edu: edu ? edu : [],
+        career: career ? career : [],
+        skill: skill ? skill : [],
+        portfolioUrl: resultData.portfolioUrl,
+        portfolioFile: fileUrl,
+        isTemporaryStorage: resultData.isTemporaryStorage,
+      };
+    }
   }
 
   async getProfileById(id: string) {
@@ -112,6 +235,16 @@ export class ProfileService {
       throw new HttpException('Profile NotFound', HttpStatus.NOT_FOUND);
 
     return profileData;
+  }
+
+  async getTempLenthByUserId(userId: string) {
+    const data = await this.profileRepository.findBy({
+      userId,
+      deletedAt: IsNull(),
+      isTemporaryStorage: true,
+    });
+
+    return data.length;
   }
 
   async deleteProfile(id: string) {
@@ -141,8 +274,9 @@ export class ProfileService {
       );
 
       if (prevRepresentative.length > 0) {
+        const idList = prevRepresentative.map((data) => data.id);
         // NOTE: 기존 대표 프로필을 기본 프로필로 변경
-        await this.profileRepository.update(prevRepresentative[0].id, {
+        await this.profileRepository.update(idList, {
           isRepresentative: false,
         });
       }
@@ -156,7 +290,7 @@ export class ProfileService {
     const eduData = {};
     const careerData = {};
 
-    edu.map((data, index) => {
+    edu?.map((data, index) => {
       eduData[`eduCategory${index + 1}`] = data.category;
       eduData[`eduSchoolName${index + 1}`] = data.schoolName;
       eduData[`eduMajor${index + 1}`] = data.major;
@@ -167,7 +301,7 @@ export class ProfileService {
         : true;
     });
 
-    career.map((data, index) => {
+    career?.map((data, index) => {
       careerData[`careerCompanyName${index + 1}`] = data.companyName;
       careerData[`careerRole${index + 1}`] = data.role;
       careerData[`careerJoinDate${index + 1}`] = data.joinDate;
@@ -201,11 +335,12 @@ export class ProfileService {
       userId: resultData.userId,
       name: resultData.name,
       isRepresentative: resultData.isRepresentative,
-      edu,
-      career,
-      skill,
+      edu: edu ? edu : [],
+      career: career ? career : [],
+      skill: skill ? skill : [],
       portfolioUrl: resultData.portfolioUrl,
       portfolioFile: fileUrl,
+      isTemporaryStorage: resultData.isTemporaryStorage,
     };
   }
 
@@ -269,6 +404,7 @@ export class ProfileService {
         skill: data.skill.split('&&'),
         portfolioUrl: data.portfolioUrl,
         portfolioFile: data.portfolioFile,
+        isTemporaryStorage: data.isTemporaryStorage,
       };
     });
 
