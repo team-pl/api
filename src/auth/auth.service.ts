e81fde3ec3016086f1f65ec24c06d2cdef63c4d5
jihyeon.lim <sen03620@naver.com>
2024-03-24 22:37:08 +0900

    새로운 카카오/네이버 소셜로그인 service 함수 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { JwtService } from '@nestjs/jwt';
 import { config } from 'dotenv';
 import * as bcrypt from 'bcrypt';
 import { User } from 'src/entity/user.entity';
+import axios from 'axios';
 
 config();
 
@@ -97,12 +98,11 @@ export class AuthService {
     naverId: string,
     name: string,
     email: string,
-    phone: string = '',
   ): Promise<{ user: User; isNewUser: boolean }> {
     let isNewUser = false;
     let user = await this.userService.getUserByNaverId(naverId);
     if (!user) {
-      user = await this.userService.naverSignUp(naverId, name, email, phone);
+      user = await this.userService.naverSignUp(naverId, name, email);
     }
     if (!user.phone) {
       isNewUser = true;
@@ -142,20 +142,99 @@ export class AuthService {
     return { accessToken, refreshToken, user, isNewUser };
   }
 
-  async getNaverJWT(
-    kakaoId: string,
-    name: string,
-    email: string,
-    phone: string,
-  ) {
+  async getNaverJWT(naverId: string, name: string, email: string) {
     const { user, isNewUser } = await this.naverValidateUser(
-      kakaoId,
+      naverId,
       name,
       email,
-      phone,
     );
     const accessToken = await this.generateAccessToken(user);
     const refreshToken = await this.generateRefreshToken(user); // refreshToken 생성
     return { accessToken, refreshToken, user, isNewUser };
+  }
+
+  // NOTE: 인가코드를 카카오에 전달 -> token 받아오기
+  async authCodeToKakao(authCode: string | string[]) {
+    const tokenUrl = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URL}&code=${authCode}&client_secret=${process.env.KAKAO_CLIENT_SECRET}`;
+
+    const response = await axios
+      .post(
+        tokenUrl,
+        {},
+        {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        },
+      )
+      .then((res) => res.data.access_token);
+
+    return response;
+  }
+
+  // NOTE: 카카오로부터 유저 정보 가져오기
+  async userDataFromKakao(token: string) {
+    const url = 'https://kapi.kakao.com/v2/user/me';
+    const response = await axios
+      .get(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => res.data);
+
+    console.log('data : ', response);
+    return response;
+  }
+
+  // NOTE: 네이버로부터 유저 정보 가져오기
+  async userDataFromNaver(token: string) {
+    const url = 'https://openapi.naver.com/v1/nid/me';
+    const response = await axios
+      .get(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => res.data);
+
+    console.log('data : ', response);
+    return response;
+  }
+
+  // NOTE: 새로운 카카오 로그인 방식
+  async loginKakao(authCode: string | string[]) {
+    const token = await this.authCodeToKakao(authCode);
+
+    const { id, kakao_account } = await this.userDataFromKakao(token);
+
+    const { email, profile } = kakao_account;
+    const { accessToken, refreshToken, user, isNewUser } =
+      await this.getKakaoJWT(id, profile.nickname, email);
+
+    return {
+      user,
+      accessToken,
+      refreshToken,
+      isNewUser,
+    };
+  }
+
+  // NOTE: 새로운 네이버 로그인 방식
+  async loginNaver(authCode: string) {
+    const token = authCode;
+
+    const { response } = await this.userDataFromNaver(token);
+
+    const { id, email, name } = response;
+    const { accessToken, refreshToken, user, isNewUser } =
+      await this.getNaverJWT(id, name, email);
+
+    return {
+      user,
+      accessToken,
+      refreshToken,
+      isNewUser,
+    };
   }
 }
