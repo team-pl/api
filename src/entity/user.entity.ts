a577fc1e34bd378167debcbd697aa4c8b5e5ce40
jihyeonlim2024 <156742782+jihyeonlim2024@users.noreply.github.com>
2024-04-14 16:28:08 +0900

    댓글 테이블 조인 제거

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -11,7 +11,6 @@ import {
 import { Profile } from './profile.entity';
 import { ESignUp } from 'src/type/user.type';
 import { Project } from './project.entity';
-import { Comment } from './comment.entity';
 
 @Entity({ name: 'user' })
 export class User {
@@ -152,8 +151,4 @@ export class User {
   @ApiProperty({ description: '사용자가 등록한 프로젝트' })
   @OneToMany(() => Project, (project) => project.user)
   project: Project[];
-
-  @ApiProperty({ description: '사용자가 남긴 댓글' })
-  @OneToMany(() => Comment, (comment) => comment.user)
-  comment: Comment[];
 }
