3b41cca124ba9823ba12a8f408735acdb1bf4a33
jihyeonlim2024 <156742782+jihyeonlim2024@users.noreply.github.com>
2024-04-14 16:26:57 +0900

    알림 service 및 등록 함수 추가

---
+++ src/notification/notification.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Notification } from 'src/entity/notification.entity';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { CreateNotificationDto } from './dto/create-notification.dto';
+
+@Injectable()
+export class NotificationsService {
+  constructor(
+    @InjectRepository(Notification)
+    private notificationRepository: Repository<Notification>,
+  ) {}
+
+  async create(data: CreateNotificationDto) {
+    const id = uuid();
+
+    const { userId, message, projectId } = data;
+
+    const notiData = await this.notificationRepository.create({
+      id,
+      userId,
+      message,
+      projectId,
+      isRead: false,
+    });
+
+    await this.notificationRepository.save(notiData);
+
+    return true;
+  }
+}
