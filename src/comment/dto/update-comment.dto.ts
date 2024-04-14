ad2ad3bbc6b2c2069bf86f729509d1fd23a57da4
jihyeonlim2024 <156742782+jihyeonlim2024@users.noreply.github.com>
2024-04-14 16:29:07 +0900

    안쓰는 함수 제거

--- src/comment/dto/update-comment.dto.ts
+++ src/comment/dto/update-comment.dto.ts
@@ -1,5 +1,5 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateCommentDto {
   @IsString()
