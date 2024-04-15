c7f80a8421c763b4d17444223a8994af102ddfc4
jihyeonlim2024 <156742782+jihyeonlim2024@users.noreply.github.com>
2024-04-15 21:54:09 +0900

    댓글 조회 API 추가

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,10 +1,35 @@
-import { Controller } from '@nestjs/common';
-import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import {
+  ApiExtraModels,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { NotificationsService } from './notification.service';
+import { GetNotificationsResDto } from './dto/response.dto';
 
 @Controller('notification')
-@ApiExtraModels()
+@ApiExtraModels(GetNotificationsResDto)
 @ApiTags('Notification')
 export class NotificationController {
   constructor(private readonly service: NotificationsService) {}
+
+  @Get()
+  @ApiOperation({ summary: '알림 조회 API' })
+  @ApiQuery({
+    name: 'userId',
+    required: true,
+    description: '사용자 ID',
+    type: 'string',
+  })
+  @ApiResponse({
+    status: 200,
+    type: GetNotificationsResDto,
+  })
+  async getNotifications(@Query('userId') userId: string) {
+    const data = await this.service.getNotifications(userId);
+
+    return { list: data };
+  }
 }
