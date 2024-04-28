06c083d0121241aed5a43ef6ec0221054b2f5029
jihyeonlim2024 <156742782+jihyeonlim2024@users.noreply.github.com>
2024-04-28 16:13:27 +0900

    feat: 프로젝트 지원 관련 API 추가

--- src/apply/apply.controller.ts
+++ src/apply/apply.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   HttpException,
   HttpStatus,
+  Param,
+  Patch,
   Post,
   Request,
   UseGuards,
@@ -11,17 +13,18 @@ import {
 import {
   ApiExtraModels,
   ApiOperation,
+  ApiParam,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { ApplyService } from './apply.service';
 import { JwtAuthGuard } from 'src/auth/jwtAuth.guard';
 import { CreateApplyDto } from './dto/create-apply.dto';
-import { PostApplyResDto } from './dto/response.dto';
+import { CheckedApplyResDto, PostApplyResDto } from './dto/response.dto';
 
 @Controller('apply')
-@ApiExtraModels(CreateApplyDto, PostApplyResDto)
-@ApiTags('Apply')
+@ApiExtraModels(CreateApplyDto, PostApplyResDto, CheckedApplyResDto)
+@ApiTags('프로젝트 지원')
 export class ApplyController {
   constructor(private readonly service: ApplyService) {}
 
@@ -31,6 +34,14 @@ export class ApplyController {
   @ApiResponse({
     status: 401,
     description: 'user ID NotFound',
+  })
+  @ApiResponse({
+    status: 409,
+    description: '프로젝트 모집이 마감되어 지원이 불가능합니다.',
+  })
+  @ApiResponse({
+    status: 422,
+    description: '프로젝트에 이미 지원하여 중복지원이 불가능합니다.',
   })
   @ApiResponse({
     status: 200,
@@ -47,5 +58,121 @@ export class ApplyController {
     }
 
     return this.service.create(data, id);
+  }
+
+  @Patch('check/:id')
+  @ApiOperation({ summary: '프로젝트 확인완료 API' })
+  @UseGuards(JwtAuthGuard)
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '프로젝트 지원 ID',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'user ID NotFound',
+  })
+  @ApiResponse({
+    status: 404,
+    description: '프로젝트에 지원한 내역이 없습니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: CheckedApplyResDto,
+  })
+  async check(@Request() req, @Param('id') applyId: string) {
+    const { id } = req.user.name;
+
+    if (!id) {
+      throw new HttpException('NotFound', HttpStatus.UNAUTHORIZED);
+    }
+
+    return this.service.check(applyId);
+  }
+
+  @Patch('confirm/:id')
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '프로젝트 지원 ID',
+  })
+  @ApiOperation({ summary: '프로젝트 참여확정 API' })
+  @UseGuards(JwtAuthGuard)
+  @ApiResponse({
+    status: 401,
+    description: 'user ID NotFound',
+  })
+  @ApiResponse({
+    status: 422,
+    description: '해당 인원은 프로젝트에 이미 참여확정되었습니다.',
+  })
+  async confirm(@Request() req, @Param('id') applyId: string) {
+    const { id } = req.user.name;
+
+    if (!id) {
+      throw new HttpException('NotFound', HttpStatus.UNAUTHORIZED);
+    }
+
+    return this.service.confirm(applyId);
+  }
+
+  @Patch('cancel/:id')
+  @ApiOperation({ summary: '프로젝트 확정취소 API' })
+  @UseGuards(JwtAuthGuard)
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '프로젝트 지원 ID',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'user ID NotFound',
+  })
+  @ApiResponse({
+    status: 422,
+    description: '해당 인원은 프로젝트에 이미 확정취소되었습니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: CheckedApplyResDto,
+  })
+  async cancel(@Request() req, @Param('id') applyId: string) {
+    const { id } = req.user.name;
+
+    if (!id) {
+      throw new HttpException('NotFound', HttpStatus.UNAUTHORIZED);
+    }
+
+    return this.service.cancel(applyId);
+  }
+
+  @Patch('reject/:id')
+  @ApiOperation({ summary: '프로젝트 거절 API' })
+  @UseGuards(JwtAuthGuard)
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '프로젝트 지원 ID',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'user ID NotFound',
+  })
+  @ApiResponse({
+    status: 422,
+    description: '해당 인원은 프로젝트에 이미 거절되었습니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: CheckedApplyResDto,
+  })
+  async reject(@Request() req, @Param('id') applyId: string) {
+    const { id } = req.user.name;
+
+    if (!id) {
+      throw new HttpException('NotFound', HttpStatus.UNAUTHORIZED);
+    }
+
+    return this.service.reject(applyId);
   }
 }
