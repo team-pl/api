1fea78603c63706449dd60f71d5ce903c6136949
jihyeonlim2024 <156742782+jihyeonlim2024@users.noreply.github.com>
2024-04-28 16:20:11 +0900

    feat: 지원 타입 추가

--- src/type/apply.type.ts
+++ src/type/apply.type.ts
@@ -1,6 +1,7 @@
 export enum EApplyState {
   UNCONFIRMED = 'UNCONFIRMED', // '미확인' 상태
+  CHECKED = 'CHECKED', // '확정 완료' 상태
   REJECT_CONFIRMED = 'REJECT_CONFIRMED', // '참여 거절' 상태
-  CONFIRMED = 'UNCONFIRMED', // '참여 확정' 상태
+  CONFIRMED = 'CONFIRMED', // '참여 확정' 상태
   CONFIRMED_CANCELED = 'CONFIRMED_CANCELED', // '확정 취소' 상태
 }
