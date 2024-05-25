9afef1168af654b7de48bb5b6ddace94c33f5fbe
jihyeonlim2024 <156742782+jihyeonlim2024@users.noreply.github.com>
2024-05-25 12:15:30 +0900

    fix: cors 관련 설정 변경

--- src/main.ts
+++ src/main.ts
@@ -51,15 +51,17 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors({
-    origin: [
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3000'
-        : 'https://teampl-plus.com',
-    ],
-    credentials: true,
-    exposedHeaders: ['Access-Control-Allow-Origin'],
-  });
+  app.enableCors();
+
+  // app.enableCors({
+  //   origin: [
+  //     process.env.NODE_ENV === 'development'
+  //       ? 'http://localhost:3000'
+  //       : 'https://teampl-plus.com',
+  //   ],
+  //   credentials: true,
+  //   exposedHeaders: ['Access-Control-Allow-Origin'],
+  // });
 
   await app.listen(process.env.NODE_ENV === 'development' ? 3001 : 3000);
 }
