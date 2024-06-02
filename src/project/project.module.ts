076cfe25803d15d5aee6af50ddf200b0694a4207
jihyeonlim2024 <156742782+jihyeonlim2024@users.noreply.github.com>
2024-06-02 13:51:01 +0900

    feat: 프로젝트 모듈>jwt module 추가

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -6,9 +6,14 @@ import { Project } from 'src/entity/project.entity';
 import { FileModule } from 'src/file/file.module';
 import { ProfileModule } from 'src/profile/profile.module';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
     TypeOrmModule.forFeature([Project]),
     FileModule,
     ProfileModule,
