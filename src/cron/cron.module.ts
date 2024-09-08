7292b7113ebfbfc10319cc3d8905d32ff81e5acc
jihyeonlim2024 <156742782+jihyeonlim2024@users.noreply.github.com>
2024-09-08 18:23:56 +0900

    feat: 프로젝트 모집 상태 업데이트 크론 작업 추가

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CronService } from './cron.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { NotificationModule } from 'src/notification/notification.module';
+import { ProjectModule } from 'src/project/project.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), NotificationModule], // 스케줄 모듈을 전역에서 사용할 수 있도록 초기화
+  imports: [ScheduleModule.forRoot(), NotificationModule, ProjectModule], // 스케줄 모듈을 전역에서 사용할 수 있도록 초기화
   providers: [CronService],
 })
 export class CronModule {}
--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { NotificationsService } from 'src/notification/notification.service';
+import { ProjectService } from 'src/project/project.service';
 
 @Injectable()
 export class CronService {
-  constructor(private notificationService: NotificationsService) {}
+  constructor(
+    private notificationService: NotificationsService,
+    private projectService: ProjectService,
+  ) {}
 
   // NOTE: 매일 1시에 한번씩 알림을 조회하여 30일이 지났는지 확인하여 자동으로 삭제
   @Cron(CronExpression.EVERY_DAY_AT_1AM)
@@ -34,5 +38,25 @@ export class CronService {
     if (deleteList.length) {
       await this.notificationService.deleteNotifications(deleteList);
     }
+
+    console.log('알림 삭제 크론 종료');
+  }
+
+  // NOTE: 매일 0시에 한번씩 프로젝트를 조회하여 마감일이 지났는지 확인하여 자동으로 상태 업데이트
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async updateProjectState() {
+    console.log('프로젝트 모집 상태 업데이트 크론 시작');
+
+    const expiredList = await this.projectService.getExpiredProjects();
+
+    const expiredIdList = expiredList.map((value) => value.id);
+
+    console.log('만료된 프로젝트>expiredIdList :', expiredIdList);
+
+    if (expiredIdList.length) {
+      await this.projectService.updateExpiredProjects(expiredIdList);
+    }
+
+    console.log('프로젝트 모집 상태 업데이트 크론 종료');
   }
 }
